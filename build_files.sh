263afbdb932e64784fe73b602b99b04d35bbdf20
joserey-alfante <joserey.alfante@ctu.edu.ph>
2023-12-24 04:00:40 +0800

    Modify the files information

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,2 @@
-source myenv/bin/activate  # Activating the virtual environment
-pip install -r requirements.txt  # Install dependencies
-python manage.py collectstatic  # Run collectstatic or any other necessary commands
+pip install -r requirements.txt
+python3.9 manage.py collectstatic
