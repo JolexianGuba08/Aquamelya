8404ddc31d126bed103920249aa0d8a2ec0e39ae
Jose Rey Alfante <joserey.alfante@ctu.edu.ph>
2023-12-24 03:53:19 +0800

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,3 @@
-pip install -r requirements.txt
-python manage.py collectstatic
+source myenv/bin/activate  # Activating the virtual environment
+pip install -r requirements.txt  # Install dependencies
+python manage.py collectstatic  # Run collectstatic or any other necessary commands
