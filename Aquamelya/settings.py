27860d4bccbb93ca57913c7c6aef27d3424d6dce
joserey-alfante <joserey.alfante@ctu.edu.ph>
2023-12-25 04:16:16 +0800

    Updated settings.py

--- Aquamelya/settings.py
+++ Aquamelya/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jr+qa24!i%@g-x0ax^acgo%l+zx3+lr!#hku)rmby6pv&dzi+('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'localhost', '.now.sh']
 
 # Application definition
