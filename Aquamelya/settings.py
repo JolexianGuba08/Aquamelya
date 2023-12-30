1c3e614bf54aadd888d9ab20b06fe5f13b9a4786
joserey-alfante <joserey.alfante@ctu.edu.ph>
2023-12-31 03:29:42 +0800

    Updated settings.py

--- Aquamelya/settings.py
+++ Aquamelya/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = 'django-insecure-jr+qa24!i%@g-x0ax^acgo%l+zx3+lr!#hku)rmby6pv&dzi+(
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['.vercel.app''.now.sh']
+ALLOWED_HOSTS = ['.vercel.app''.now.sh', '*']
 
 # Application definition
 
